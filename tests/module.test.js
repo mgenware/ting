e4725f0019736f99b7508d42c7b630689581ebd5
Mgen <m@mgenware.com>
2018-05-09 18:45:28 +0800

    Use a separate file for default sanitize-html options

--- tests/module.test.js
+++ tests/module.test.js
@@ -3,6 +3,6 @@ const main = require('..');
 describe('require this module', () => {
   test('Verify module members', () => {
     expect(typeof main.sanitize).toBe('function');
-    expect(typeof main.defaultOptions).toBe('object');
+    expect(typeof main.defaultSanitizeHTMLOptions).toBe('object');
   });
 });
