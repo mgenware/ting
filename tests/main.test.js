e0623d882e79440b7a54ebebea7f1c804f1c1a2a
Mgen <m@mgenware.com>
2018-05-09 19:07:34 +0800

    Add test for code html

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,6 +1,12 @@
 const main = require('..');
-
+const fs = require('fs');
+const { promisify } = require('util');
+const readFileAsync = promisify(fs.readFile);
 const { sanitize } = main;
+
+async function readTestFileAsync(name) {
+  return await readFileAsync(`./tests/data/${name}.html`, 'utf8');
+}
 
 test('<img>', () => {
   expect(sanitize('<img src="a" abc="1" width="23" height="30" />')).toBe('<img src="a" width="23" height="30" />');
@@ -16,4 +22,9 @@ test('Override options', () => {
     opts.allowedAttributes.iframe = ['src'];
     return opts;
   })).toBe('<iframe src="https://coldfunction.com"></iframe>');
+});
+
+test('Code', async () => {
+  const html = await readTestFileAsync('code');
+  expect(sanitize(html)).toBe(html);
 });
